814cce96ca9015b3b04384aa54acc907f531fe57
Ocvcorp <ocvcorp123@outlook.com>
2020-07-28 23:32:14 +0300

    Изменения

---
+++ KuraevGeometryClassLibrary/Shape2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KuraevGeometryClassLibrary
+{
+    public abstract class Shape2D
+    {
+        public Shape2D(string type)
+        {
+            Type = type;
+        }
+        public string Type { get; }
+        public abstract double GetArea();
+    }
+}
---
+++ KuraevGeometryClassLibrary/Shapes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace KuraevGeometryClassLibrary
+{
+
+    public class Triangle2D : Shape2D
+    {
+        public Triangle2D(double a, double b, double c) : base("Triangle")
+        {
+            if ((c >= a + b) || (b >= a + c) || (a >= c + b))
+                throw new Exception("Неверный ввод длин сторон");
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            if ((c * c == a * a + b * b) || (a * a == c * c + b * b) || (b * b == c * c + a * a))
+                IsRectangular = true;
+        }
+        public bool IsRectangular { get; }
+        private double a, b, c;
+        public override double GetArea()
+        {
+            double area;
+            double p = a + b + c;
+            area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+    }
+
+
+    public class Circle2D : Shape2D
+    {
+        public Circle2D(double radius) : base("Circle")
+        {
+            this.radius =  radius;
+        }
+        private double radius;
+        public override double GetArea()
+        {
+            double area;
+            double p = a + b + c;
+            area = Math.PI * radius * radius;
+            return area;
+        }
+    }
+}
