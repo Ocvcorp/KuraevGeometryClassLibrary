7c926f9c74ee6b357004a7d4121fd74bed8a35eb
Ocvcorp <ocvcorp123@outlook.com>
2020-08-01 11:51:42 +0300

    edit

--- UnitTestGeometry/UnitTestShapes.cs
+++ UnitTestGeometry/UnitTestShapes.cs
@@ -1,9 +1,7 @@
 using KuraevGeometryClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System;
-using System.Collections.Generic;
 using Assert = NUnit.Framework.Assert;
 
 namespace UnitTestGeometry
@@ -14,19 +12,21 @@ namespace UnitTestGeometry
         [TestMethod]
         public void TestCalcTriangle()
         {
-            //Прямоугольный треугольник
+            //right triangle
             Triangle2D t1 = new Triangle2D(3, 4, 5);
             double area = t1.GetArea();
             bool isRectangular = t1.IsRectangular;
             Assert.AreEqual(6, area);
             Assert.AreEqual(true, isRectangular);
-            //Непрямоугольный треугольник
+
+            //non-rectangular triangle
             Triangle2D t2 = new Triangle2D(5, 5, 8);
             area = t2.GetArea();
             isRectangular = t2.IsRectangular;
             Assert.AreEqual(12, area);
             Assert.AreEqual(false, isRectangular);
-            //Неверное сочетание сторон
+            
+            //incorrect input
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.ThrowsException<Exception>(() => new Triangle2D(5, 10, 5));
         }
         [TestMethod]
@@ -40,14 +40,15 @@ namespace UnitTestGeometry
         [TestMethod]
         public void GetAreas()
         {
-            //расчет площади без указания типа
+            //area caclulation without type
             Shape2D[] shapes = {
                 new Triangle2D(3, 4, 5),
                 new Circle2D(2),
                 new Circle2D(3),
                 new Triangle2D(8, 12, 6)
             };
-            //посчитаем сумму площадей
+            
+            //area sum calculation
             double areaSum = .0;
             foreach (var sh in shapes)
                 areaSum += sh.GetArea();
